5938f54d7d8e74559516d24b96727ae8f2db749f
Roland Schütz <mail@rolandschuetz.at>
2012-03-24 03:13:05 +0100

    Simplifying project, changing startpage

---
+++ output/tree.js
@@ -0,0 +1,25 @@
+Docs.classData = {
+    "text": "API Documentation",
+    "iconCls": "icon-docs",
+    "id": "apidocs",
+    "children": [{
+        "text": "User",
+        "iconCls": "icon-pkg",
+        "url": "/api/Bancha.data.Model",
+        "leaf": true
+    }, {
+        "text": "Article",
+        "iconCls": "icon-pkg",
+        "url": "/api/Bancha.data.Model",
+        "leaf": true
+    }]
+};
+
+
+/* propaply not needed
+Docs.icons = {
+    "/api/Bancha": "icon-singleton",
+    "/api/Bancha.data.Model": "icon-class",
+    "/api/Ext.data.validations": "icon-class"
+};
+*/
