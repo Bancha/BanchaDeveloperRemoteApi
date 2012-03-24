9284d375afc0d320a487c44a7ef148ee3fdbe453
Roland Schütz <mail@rolandschuetz.at>
2012-03-24 02:44:12 +0100

    Adding bancha-specific favorite-less jsduck-build

---
+++ app/Application.js
@@ -0,0 +1,40 @@
+/**
+ * Main application definition for Docs app.
+ *
+ * We define our own Application class because this way we can also
+ * easily define the dependencies.
+ */
+Ext.define('Docs.Application', {
+    extend: 'Ext.app.Application',
+    name: 'Docs',
+
+    requires: [
+        'Docs.History',
+        'Docs.Settings'
+    ],
+
+    uses: [
+        'Ext.util.History',
+        'Ext.data.JsonP'
+    ],
+
+    controllers: [
+        'Classes',
+        'Search'
+    ],
+
+    launch: function() {
+        Docs.App = this;
+        Docs.Settings.init();
+
+        Ext.create('Docs.view.Viewport');
+
+        Docs.History.init();
+
+        // When google analytics event tracking script present on page
+        if (Docs.initEventTracking) {
+            Docs.initEventTracking();
+        }
+    }
+
+});
---
+++ app/view/Viewport.js
@@ -0,0 +1,178 @@
+/**
+ * The main viewport, split in to a west and center region.
+ * The North region should also be shown by default in the packaged
+ * (non-live) version of the docs. TODO: close button on north region.
+ */
+Ext.define('Docs.view.Viewport', {
+    extend: 'Ext.container.Viewport',
+    requires: [
+        'Docs.view.cls.Header',
+        'Docs.view.cls.Overview',
+        'Docs.view.index.Container',
+        'Docs.view.tree.Tree',
+        'Docs.view.ClassGrid',
+        'Docs.Settings',
+        'Docs.History'
+    ],
+
+    id: 'viewport',
+    layout: 'border',
+    defaults: { xtype: 'container' },
+
+    initComponent: function() {
+        this.items = [
+
+            {
+                region:'west',
+                width: 240,
+                id: 'west-region-container',
+                padding: '5 0 0 0',
+                layout: 'vbox',
+                defaults: {
+                    xtype: 'container',
+                    width: "100%"
+                },
+                items: [
+                    {
+                        xtype: 'button',
+                        cls: 'logo',
+                        height: 60,
+                        margin: '0 0 10 10',
+                        width: 220,
+                        border: 0,
+                        ui: 'hmm',
+                        listeners: {
+                            click: function() {
+                                Docs.App.getController('Classes').loadIndex();
+                            },
+                            scope: this
+                        }
+                    },
+                    {
+                        cls: 'search',
+                        id: 'search-container',
+                        margin: '0 0 0 5',
+                        height: 40,
+                        items: [
+                            {
+                                xtype: 'triggerfield',
+                                triggerCls: 'reset',
+                                emptyText: 'Search',
+                                id: 'search-field',
+                                enableKeyEvents: true,
+                                hideTrigger: true,
+                                onTriggerClick: function() {
+                                    this.reset();
+                                    this.focus();
+                                    this.setHideTrigger(true);
+                                    Ext.getCmp('search-dropdown').hide();
+                                }
+                            },
+                            {
+                                xtype: 'searchdropdown'
+                            }
+                        ]
+                    },
+                    {
+                        id: 'nested-west-region-container',
+                        flex: 1,
+                        layout: 'border',
+                        border: false,
+                        items: [
+                            {
+                                region: 'center',
+                                xtype: 'classtree',
+                                padding: '5 10 0 10',
+                                margin: '0 5 2 0',
+                                root: Docs.classData
+                                // dockedItems: [{
+                                //     xtype: 'toolbar',
+                                //     baseCls: null,
+                                //     dock: 'top',
+                                //     padding: '0 0 5 0',
+                                //     items: [
+                                //         {
+                                //             xtype: 'button',
+                                //             text: 'Sort by Package',
+                                //             menu: [
+                                //                 { text: 'by Category' },
+                                //                 { text: 'by Hierarchy' },
+                                //                 { text: 'by Popularity' }
+                                //             ]
+                                //         },
+                                //         { xtype: 'tbfill'},
+                                //         {
+                                //             xtype: 'button',
+                                //             iconCls: 'expandAllMembers',
+                                //             tooltip: "Expand all"
+                                //         }
+                                //     ]
+                                // }]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                region: 'center',
+                id: 'center-container',
+                layout: 'fit',
+                minWidth: 800,
+                padding: '20 20 5 0',
+                items: {
+                    id: 'card-panel',
+                    cls: 'card-panel',
+                    xtype: 'container',
+                    layout: 'card',
+                    padding: '20',
+                    items: [
+                        {
+                            autoScroll: true,
+                            xtype: 'indexcontainer'
+                        },
+                        {
+                            xtype: 'container',
+                            layout: {
+                                type: 'vbox',
+                                align: 'stretch'
+                            },
+                            items: [
+                                {xtype: 'classheader'},
+                                {xtype: 'classoverview', flex: 1}
+                            ]
+                        },
+                        {
+                            autoScroll: true,
+                            xtype: 'container',
+                            id: 'guide'
+                        },
+                        {
+                            xtype: 'container',
+                            id: 'failure'
+                        }
+                    ]
+                }
+            },
+            {
+                region: 'south',
+                id: 'footer',
+                contentEl: 'footer-content',
+                height: 15
+            }
+        ];
+
+        this.callParent(arguments);
+    },
+
+    /**
+     * Sets the contents of `<title>` tag.
+     * @param {String} text
+     */
+    setPageTitle: function(text) {
+        text = Ext.util.Format.stripTags(text);
+        if (!this.origTitle) {
+            this.origTitle = document.title;
+        }
+        document.title = text ? (text + " - " + this.origTitle) : this.origTitle;
+    }
+});
