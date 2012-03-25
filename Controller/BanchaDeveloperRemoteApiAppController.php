d153d4fc44f8970290f020c56bb2dbf43f9a167c
Roland Schütz <mail@rolandschuetz.at>
2012-03-25 23:04:41 +0200

    Even more stuff for getting a full plugin

---
+++ Controller/BanchaDeveloperRemoteApiAppController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * BanchaDeveloperRemoteApiAppController file.
+ *
+ * @package    BanchaDeveloperRemoteApi
+ * @subpackage Controller
+ */
+
+/**
+ * BanchaDeveloperRemoteApiAppController.
+ *
+ * @package    BanchaDeveloperRemoteApi
+ * @subpackage Controller
+ */
+class BanchaDeveloperRemoteApiAppController extends AppController {
+
+}
+
+?>
